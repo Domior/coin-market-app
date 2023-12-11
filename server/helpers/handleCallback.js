e24edfb15e2bf07796c0f14cf4a0a51a108649c4
Snisar Andrii <andrew.snisar@gmail.com>
2023-12-11 13:24:34 +0200

    created request func wrapper

---
+++ server/helpers/handleCallback.js
@@ -0,0 +1,22 @@
+const STATUSES = require('../constants/statuses');
+const { ERRORS } = require('../constants/text');
+const handleError = require('../helpers/handleError');
+
+module.exports = callback => {
+  return async (req, res, next) => {
+    try {
+      await callback(req, res, next);
+    } catch (error) {
+      if (error.response && error.response.status === STATUSES.TOO_MANY_REQUESTS)
+        return handleError(
+          res,
+          STATUSES.TOO_MANY_REQUESTS,
+          ERRORS.TOO_MANY_REQUESTS,
+        );
+
+      if (error.code && error.message) return handleError(res, code, message);
+
+      handleError(res, STATUSES.INTERNAL_SERVER_ERROR, ERRORS.INTERNAL_SERVER_ERROR);
+    }
+  };
+};
--- server/routes/appRoutes.js
+++ server/routes/appRoutes.js
@@ -3,33 +3,43 @@ const router = express.Router();
 const axios = require('axios');
 
 const auth = require('../auth');
-const handleError = require('../helpers/handleError');
 const FavoritesModel = require('../models/Favorites');
 const STATUSES = require('../constants/statuses');
 const REQUEST_DEFAULT_PARAMS = require('../constants/params');
-const { ERRORS } = require('../constants/text');
 const DBService = require('../services/DBService');
+const handleCallback = require('../helpers/handleCallback');
 
 const favoritesModelService = new DBService(FavoritesModel);
 
-router.get('/coins', auth, async (req, res) => {
-  const { vs_currency = REQUEST_DEFAULT_PARAMS.vs_currency } = req.body;
-  const { email, metamaskAddress } = req.user;
+router.get(
+  '/coins',
+  auth,
+  handleCallback(async (req, res) => {
+    const { vs_currency = REQUEST_DEFAULT_PARAMS.vs_currency } = req.body;
+    const { email, metamaskAddress } = req.user;
 
-  const params = {
-    vs_currency,
-    order: REQUEST_DEFAULT_PARAMS.order,
-    per_page: REQUEST_DEFAULT_PARAMS.per_page,
-    page: REQUEST_DEFAULT_PARAMS.page,
-  };
+    const params = {
+      vs_currency,
+      order: REQUEST_DEFAULT_PARAMS.order,
+      per_page: REQUEST_DEFAULT_PARAMS.per_page,
+      page: REQUEST_DEFAULT_PARAMS.page,
+    };
 
-  try {
-    const { data } = await axios.get(`${process.env.COINGECKO_URL}/coins/markets`, { params });
+    const { data } = await axios.get(`${process.env.COINGECKO_URL}/coins/markets`, {
+      params,
+    });
 
-    const item = await favoritesModelService.findFavorites({ email, metamaskAddress });
+    const item = await favoritesModelService.findFavorites({
+      email,
+      metamaskAddress,
+    });
 
     if (!item) {
-      await favoritesModelService.createFavorites({ email, metamaskAddress, favorites: [] });
+      await favoritesModelService.createFavorites({
+        email,
+        metamaskAddress,
+        favorites: [],
+      });
     } else {
       const newData = data.map(coin => {
         return {
@@ -41,32 +51,31 @@ router.get('/coins', auth, async (req, res) => {
     }
 
     return res.status(STATUSES.OK).json({ data });
-  } catch (error) {
-    console.log(error);
-    if (error.response.status === STATUSES.TOO_MANY_REQUESTS) return handleError(res, STATUSES.TOO_MANY_REQUESTS, ERRORS.TOO_MANY_REQUESTS);
-    handleError(res, STATUSES.INTERNAL_SERVER_ERROR, ERRORS.INTERNAL_SERVER_ERROR);
-  }
-});
+  }),
+);
 
-router.get('/coins/:id', auth, async (req, res) => {
-  const { id } = req.params;
+router.get(
+  '/coins/:id',
+  auth,
+  handleCallback(async (req, res) => {
+    const { id } = req.params;
 
-  try {
     const { data } = await axios.get(`${process.env.COINGECKO_URL}/coins/${id}`);
     res.status(STATUSES.OK).json({ data });
-  } catch (error) {
-    console.log(error);
-    if (error.response.status === STATUSES.TOO_MANY_REQUESTS) return handleError(res, STATUSES.TOO_MANY_REQUESTS, ERRORS.TOO_MANY_REQUESTS);
-    handleError(res, STATUSES.INTERNAL_SERVER_ERROR, ERRORS.INTERNAL_SERVER_ERROR);
-  }
-});
+  }),
+);
 
-router.patch('/favorites/:coinId', auth, async (req, res) => {
-  const { coinId } = req.params;
-  const { email, metamaskAddress } = req.user;
+router.patch(
+  '/favorites/:coinId',
+  auth,
+  handleCallback(async (req, res) => {
+    const { coinId } = req.params;
+    const { email, metamaskAddress } = req.user;
 
-  try {
-    const item = await favoritesModelService.findFavorites({ email, metamaskAddress });
+    const item = await favoritesModelService.findFavorites({
+      email,
+      metamaskAddress,
+    });
 
     if (item) {
       if (!item.favorites.includes(coinId)) {
@@ -77,32 +86,32 @@ router.patch('/favorites/:coinId', auth, async (req, res) => {
         await item.save();
       }
     } else {
-      await favoritesModelService.createFavorites({ email, metamaskAddress, favorites: [coinId] });
+      await favoritesModelService.createFavorites({
+        email,
+        metamaskAddress,
+        favorites: [coinId],
+      });
     }
 
     res.status(STATUSES.OK).json({ coinId });
-  } catch (error) {
-    console.log(error);
-    handleError(res, STATUSES.INTERNAL_SERVER_ERROR, ERRORS.INTERNAL_SERVER_ERROR);
-  }
-});
+  }),
+);
 
-router.get('/chart/:id', auth, async (req, res) => {
-  const { id } = req.params;
-  const { vs_currency = REQUEST_DEFAULT_PARAMS.vs_currency, days = REQUEST_DEFAULT_PARAMS.days } = req.query;
+router.get(
+  '/chart/:id',
+  auth,
+  handleCallback(async (req, res) => {
+    const { id } = req.params;
+    const { vs_currency = REQUEST_DEFAULT_PARAMS.vs_currency, days = REQUEST_DEFAULT_PARAMS.days } = req.query;
 
-  const params = {
-    vs_currency,
-    days,
-  };
+    const params = {
+      vs_currency,
+      days,
+    };
 
-  try {
     const { data } = await axios.get(`${process.env.COINGECKO_URL}/coins/${id}/market_chart`, { params });
     res.status(STATUSES.OK).json({ data });
-  } catch (error) {
-    console.log(error);
-    handleError(res, STATUSES.INTERNAL_SERVER_ERROR, ERRORS.INTERNAL_SERVER_ERROR);
-  }
-});
+  }),
+);
 
 module.exports = router;
